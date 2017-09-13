326f57593485b750401e7a2cbd3387644fbd919d
Sachin Saluja <sachin.saluja132@gmail.com>
2017-09-13 09:57:28 +0200

    Speed up animation

--- src/notification-bar.component.ts
+++ src/notification-bar.component.ts
@@ -94,7 +94,7 @@ export const MESSAGES_CONFIG = new OpaqueToken('notification-bar.messages.config
                     opacity: 0,
                     transform: 'translateY(-100%)'
                 }),
-                animate('600ms ease-in')
+                animate('200ms ease-in')
             ]),
             transition('* => void', [
                 animate('200ms ease-out', style({
